544762af4a72f0ec6b7e61fa0684a365b6d404ed
VinnyMendes <vinypotter.vm@gmail.com>
2021-12-02 13:20:38 -0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,27 @@
 import { Link } from 'react-router-dom'
+import { Navbar, Container, Nav } from 'react-bootstrap'
+import styles from './Header.module.scss'
 
 export default function Header(){
     return(
-        <header>
-            <nav>
-                <img src="" alt="logo" />
-                <Link to="/">Início</Link>
-                <Link to="/produtos">Produtos</Link>
-                <Link to="/carrinho">Carrinho</Link>
-                <Link to="/sobre">Sobre</Link>
-            </nav>
+        <header className={styles.header}>
+            <Navbar className={styles.navbar} collapseOnSelect expand="md" bg="dark" variant="dark">
+                <Container>
+                <Navbar.Brand className={styles.logo} href="/">
+                    <img src="./images/twitter.png" className={styles.image} alt="logo" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className={`me-auto ${styles.links}`}>
+                        <Link className={styles.link} to="/">Início</Link>
+                        <Link className={styles.link} to="/produtos">Produtos</Link>
+                        <Link className={styles.link} to="/carrinho">Carrinho</Link>
+                        <Link className={styles.link} to="/sobre">Sobre</Link>
+                        <Link className={styles.link} to="/carrinho">Carrinho</Link>
+                    </Nav>
+                </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </header>
     )
 }
