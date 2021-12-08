0e804b37a5c987a6a911dd913cc32bd96db4e5e3
mdaffonso <mdaffonso@gmail.com>
2021-12-08 15:11:17 -0300

    categorias hover

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -78,27 +78,29 @@ export default function Produtos(){
             <Meta title="Produtos" />
             <Container>
                 <h1 className={styles.h1}>Produtos {categoria ? <>- {traduzir(categoria)}</> : ''}</h1>
-                <Wrapper>
-                    {
-                        errors.categories ? <Col sm={12} md={3} xl={2}><p>{errors.categories}</p></Col>
-                        : loading ? <SmallSpinner />
-                        : <Categorias categorias={categories} />
-                    }
-                    {
-                            loading ? (<SmallSpinner />) :(
-                            <Row className={styles.linha}>
+                { 
+                    loading ? <SmallSpinner /> 
+                    : (
+                        <Wrapper>
                             {
-                                errors.products ? (<Col sm={12}><p>{errors.products}</p></Col>)
-                                : products && products.map(({id, title, price, image}) => (
-                                    <Col sm={12} md={6} lg={4} xl={3} key={id}>
-                                        <Produto identidade={id} title={title} price={price} image={image} />
-                                    </Col>
-                                ))
+                                errors.categories ? <Col sm={12} md={3} xl={2}><p>{errors.categories}</p></Col>
+                                : <Categorias categorias={categories} />
                             }
-                            </Row>
-                        )
-                    }
-                </Wrapper>
+                            {
+                                <Row className={styles.linha}>
+                                {
+                                    errors.products ? (<Col sm={12}><p>{errors.products}</p></Col>)
+                                    : products && products.map(({id, title, price, image}) => (
+                                        <Col sm={12} md={6} lg={4} xl={3} key={id}>
+                                            <Produto identidade={id} title={title} price={price} image={image} />
+                                        </Col>
+                                    ))
+                                }
+                                </Row>
+                            }
+                        </Wrapper>
+                    )
+                }
             </Container>
         </>
     )
